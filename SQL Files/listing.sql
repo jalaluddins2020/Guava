42dc7661ef0bbbfa2e87b90dae0eca98208d4b32
gadddy <gadman.tang.2020@smu.edu.sg>
2022-04-01 12:43:17 +0800

    New SQL

--- SQL Files/listing.sql
+++ SQL Files/listing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Mar 31, 2022 at 09:00 AM
+-- Generation Time: Apr 01, 2022 at 04:42 AM
 -- Server version: 5.7.34
 -- PHP Version: 7.4.21
 
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `listing`
 --
-CREATE DATABASE IF NOT EXISTS `listing` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `listing`;
 
 -- --------------------------------------------------------
 
@@ -30,45 +28,28 @@ USE `listing`;
 --
 
 DROP TABLE IF EXISTS `listing`;
-CREATE TABLE `listing` (
-  `listingID` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `listing` (
+  `listingID` int(11) NOT NULL AUTO_INCREMENT,
   `customerID` int(11) NOT NULL,
   `talentID` int(11) DEFAULT NULL,
   `name` varchar(300) NOT NULL,
   `details` varchar(300) NOT NULL,
   `status` varchar(300) NOT NULL,
   `price` float(6,2) NOT NULL,
-  `paymentStatus` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `paymentStatus` varchar(300) NOT NULL,
+  `dateCreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`listingID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `listing`
 --
 
-INSERT INTO `listing` (`listingID`, `customerID`, `talentID`, `name`, `details`, `status`, `price`, `paymentStatus`) VALUES
-(1, 1, 2, 'Wedding Photoshoot', 'Require a wedding photographer!', 'Accepted', 100.00, 'unpaid'),
-(2, 2, NULL, 'Professional Photographer', 'Looking for a professional photographer that can help my company take some quality photos!', 'available', 300.00, 'unpaid'),
-(3, 1, NULL, 'Picnic Photographer', 'Have an upcoming picnic even this weekend with my family. Looking for a photographer that is willing to tag along and document it!', 'available', 50.00, 'unpaid');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `listing`
---
-ALTER TABLE `listing`
-  ADD PRIMARY KEY (`listingID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `listing`
---
-ALTER TABLE `listing`
-  MODIFY `listingID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+INSERT INTO `listing` (`listingID`, `customerID`, `talentID`, `name`, `details`, `status`, `price`, `paymentStatus`, `dateCreated`) VALUES
+(1, 1, 2, 'Wedding Photoshoot', 'Require a wedding photographer!', 'Accepted', 100.00, 'unpaid', '2022-03-27 05:02:11'),
+(2, 2, NULL, 'Professional Photographer', 'Looking for a professional photographer that can help my company take some quality photos!', 'available', 300.00, 'unpaid', '2022-03-27 23:08:23'),
+(3, 1, NULL, 'Picnic Photographer', 'Have an upcoming picnic even this weekend with my family. Looking for a photographer that is willing to tag along and document it!', 'available', 50.00, 'unpaid', '2022-03-29 06:40:31'),
+(4, 2, NULL, 'Skate Photographer', 'Hi there, I am currently looking for a photographer that can preferably skate while taking photographs of my team. Do accept if you are interested!', 'available', 80.00, '', '2022-03-31 09:03:45');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
