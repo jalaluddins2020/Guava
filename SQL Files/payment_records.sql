b3b6e2170970790b160fa4b55b71950648636532
ryanyakx <92264236+ryanyakx@users.noreply.github.com>
2022-04-04 13:24:06 +0800

    data change

--- SQL Files/payment_records.sql
+++ SQL Files/payment_records.sql
@@ -7,11 +7,12 @@ CREATE TABLE IF NOT EXISTS payments (
     payment_id  VARCHAR(128) NOT NULL,
     listing_id   INT  NOT NULL,
     customer_id  INT  NOT NULL,
+    talent_id INT NOT NULL,
     price   FLOAT(10,2)   NOT NULL,
     date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY(listing_id)
 );
 
-INSERT INTO payments (payment_id, listing_id, customer_id, price, date_time) VALUES ("PAYID-MI5PAJI63V0799180830831E", 7, 5, 15.00, "2022-03-17 17:54:32");
-INSERT INTO payments (payment_id, listing_id, customer_id, price, date_time) VALUES ("PAYID-MI5O6WA2DT331953U1639052", 8, 3, 10.00, "2022-03-20 23:54:45");
+INSERT INTO payments (payment_id, listing_id, customer_id, talent_id price, date_time) VALUES ("PAYID-MI5PAJI63V0799180830831E", 7, 5, 2, 15.00, "2022-03-17 17:54:32");
+INSERT INTO payments (payment_id, listing_id, customer_id, talent_id, price, date_time) VALUES ("PAYID-MI5O6WA2DT331953U1639052", 8, 3, 1, 10.00, "2022-03-20 23:54:45");
